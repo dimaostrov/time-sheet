9e7ecf355bd725be85cb8ae92de4ee4cb29b5672
Dmitriy Ostrovskiy <dmtrostrovskiy@gmail.com>
2018-04-04 21:38:12 -0700

    Add button to make it send

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ var config = {
     storageBucket: "privet-83a48.appspot.com",
     messagingSenderId: "864513816118"
   };
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
 const database = firebase.database();
 var count = 1
 
-$('#submit').on('click', function(){
+$('#addEmployeeBtn').on('click', function(){
     var name = $('#employeeNameInput').val(); 
     var role = $('#roleInput').val();
     var startDate = $('#startInput').val();
@@ -19,7 +19,8 @@ $('#submit').on('click', function(){
     var monthsTotal = parseInt(moment(startDate, "MMDDYY").fromNow().split(' ')[0]) * 12;
     var totalBilled = monthsTotal * monthlySalary;
     
-    database.push({
+    let newPostRef = database.ref('/child')
+    database.ref().set({
         name: name,
         role: role,
         startDate: startDate,
